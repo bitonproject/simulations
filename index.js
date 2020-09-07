d4073ecb0c43b3b0a03b5463f25f9b20b66a154e
Marios Isaakidis <prometheas@autistici.org>
2020-09-07 21:33:25 +0300

    Add Watts–Strogatz small-world graph generator

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ const bitonTopology = require('./topology')
 const seedrandom = require('seedrandom')
 
 const topology = new bitonTopology(null, new seedrandom('topology seed'))
+
+// Generate honest nodes topology
+topology.resetPrng(new seedrandom('honest small world'))
+const honestSW = topology.genSmallWorld(4000, 12, 0.25, 'honest')
+
+// Generate malicious nodes topology
+topology.resetPrng(new seedrandom('malicious small world'))
+const maliciousSW = topology.genSmallWorld(1000, 12, 0.25, 'malicious')
--- topology/index.js
+++ topology/index.js
@@ -9,7 +9,7 @@ const graphology = require('graphology')
  * 1) We generate two disconnected random small-world graphs according to the Watts–Strogatz model,
  * one for honest nodes and the other for malicious nodes.
  * 2) Each node is assigned a random address in the biton overlay.
- * 3) We tweak the topology so that its degree distribution follows a power law (scale-free network).
+ * 3) We generate a scale-free overlay topology (the degree distribution follows a power law).
  * To do so we add links among the existing nodes according to a preferential attachment rule
  * biased towards super-nodes in overlapping overlay zones, inspired by the Barabási–Albert model.
  *
@@ -24,7 +24,7 @@ const graphology = require('graphology')
 class bitonTopology {
 
   constructor (initGraph, prng) {
-    if (initGraph && !(initGraph instanceof graphology) ) {
+    if (initGraph && !(initGraph instanceof graphology)) {
       throw new Error('provided initGraph is not a graphology instance')
     }
     this.graph = initGraph || new graphology({allowSelfLoops: false, type: 'undirected'})
@@ -33,6 +33,83 @@ class bitonTopology {
 
   resetPrng (prng) {
     this.prng = prng
+  }
+
+  /**
+   * Merge the provided graph into the current topology
+   * If a node or edge exists with the same name, then merge their attributes
+   *
+   * @param {graphology} graph - The graph to be merged
+   */
+  mergeGraph(graph) {
+    graph.forEachNode((node, attributes) => {
+      this.graph.mergeNode(node, attributes)
+    })
+
+    graph.forEachEdge((edge, attributes, source, target) => {
+      this.graph.mergeEdge(source, target, attributes)
+    })
+  }
+
+  /**
+   * Return a Watts–Strogatz small-world graph.
+   *
+   * @param {number} n - The number of nodes
+   * @param {number} k - The mean degree of the graph
+   * @param {number} p - The probability β of rewiring an edge
+   * @param {string} nodePrefix - Prefix for the key referencing the node
+   * @param {Object} nodeAttrs - Optional node attributes
+   * @param {Object} edgeAttrs - Optional edge attributes
+   */
+  genSmallWorld(n, k, p, nodePrefix, nodeAttrs, edgeAttrs) {
+    if (k % 2 === 1 || !(Number.isInteger(k))) {
+      throw new Error('k must be an even integer')
+    }
+
+    if (k > n) {
+      throw new Error('k cannot be greater than n')
+    }
+
+    if (k < Math.log(n)) {
+      throw new Error('k must be much greater that ln(n)')
+    }
+
+    if (p < 0 || p > 1) {
+      throw new Error('Provided probability β is invalid')
+    }
+
+    // Generate a graph and add n nodes
+    let G = new graphology({allowSelfLoops: false, type: 'undirected'})
+
+    let keyPrefix = nodePrefix || 'nodeWS'
+    for (let i = 0; i < n; i++) {
+      G.addNode(keyPrefix + i, nodeAttrs)
+    }
+
+    // Connect each node to k/2 rightmost neighbors (wrapping around the array)
+    // in effect forming a k-regular ring lattice
+    for (let i = 0; i < n; i++) {
+      for (let j = 1; j < k/2 + 1; j++) {
+        G.addEdge(keyPrefix + i, keyPrefix + ((i + j) % n), edgeAttrs)
+      }
+    }
+
+    // Rewire edges with probability β
+    const nodes = G.nodes()
+    G.forEachEdge((edge, attributes, source, target) => {
+      if (this.prng() < p) {
+        // Pick a uniformly random node, avoiding self-loops and link duplication
+        let newTarget
+        do {
+          newTarget = nodes[Math.floor(this.prng() * nodes.length)]
+        } while (source === newTarget || G.hasEdge(source, newTarget))
+
+        G.dropEdge(edge)
+        G.addEdge(source, newTarget, edgeAttrs)
+      }
+    })
+
+    return G
   }
 
 }
