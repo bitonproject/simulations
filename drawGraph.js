e864f8250d848533c6d063b484ceda9163dfb850
Marios Isaakidis <prometheas@autistici.org>
2020-09-18 13:15:06 +0300

    Drag d3 graph nodes

--- drawGraph.js
+++ drawGraph.js
@@ -59,6 +59,7 @@ class drawGraph {
       .data(this._nodes)
       .join("circle")
       .attr("r", 5)
+      .call(this.drag(this._simulation))
 
     node.append('title')
       .text(d => d.id);
@@ -75,6 +76,31 @@ class drawGraph {
           .attr("cx", d => d.x)
           .attr("cy", d => d.y)
       } )
+  }
+
+  drag (simulation) {
+
+    function dragstarted(event) {
+      if (!event.active) simulation.alphaTarget(0.3).restart()
+      event.subject.fx = event.subject.x
+      event.subject.fy = event.subject.y
+    }
+
+    function dragged(event) {
+      event.subject.fx = event.x
+      event.subject.fy = event.y
+    }
+
+    function dragended(event) {
+      if (!event.active) simulation.alphaTarget(0)
+      event.subject.fx = null
+      event.subject.fy = null
+    }
+
+    return d3.drag()
+      .on("start", dragstarted)
+      .on("drag", dragged)
+      .on("end", dragended)
   }
 
   nodeAdded (key, attributes) {
